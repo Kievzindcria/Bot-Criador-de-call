21874035864723028544103e2ca72a410926ad7b
Kiev <sf2943642@gmail.com>
2025-02-13 14:35:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -45,15 +45,15 @@ client.on('messageCreate', async (message) => {
                 .setCustomId('select_call_size')
                 .setPlaceholder('Selecione o número de espaços na call')
                 .addOptions([
-                    { label: '2 Espaços', value: '2' },
-                    { label: '3 Espaços', value: '3' },
-                    { label: '4 Espaços', value: '4' },
-                    { label: '5 Espaços', value: '5' },
-                    { label: '10 Espaços', value: '10' },
-                    { label: '15 Espaços', value: '15' },
-                    { label: '20 Espaços', value: '20' },
-                    { label: '25 Espaços', value: '25' },
-                    { label: '30 Espaços', value: '30' },
+                    { label: '2', value: '2' },
+                    { label: '3', value: '3' },
+                    { label: '4', value: '4' },
+                    { label: '5', value: '5' },
+                    { label: '10', value: '10' },
+                    { label: '15', value: '15' },
+                    { label: '20', value: '20' },
+                    { label: '25', value: '25' },
+                    { label: '30', value: '30' },
                 ])
         );
 
